b61c7a83637a1d03684f3722bfc2ce7814c473b7
guoheming <guoheming@zhinantech.com>
2017-04-07 11:22:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RequestService
+Study with Stay(www.stay4it.com). This is the "Build a http request utils by yourself".
---
+++ app/src/main/java/top/elune/demo/requestservice/v1/exceptions/CannotSupportRequestMethodException.java
@@ -0,0 +1,4 @@
+package top.elune.demo.requestservice.v1.exceptions;
+
+public class CannotSupportRequestMethodException extends IllegalArgumentException {
+}
